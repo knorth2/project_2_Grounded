e1d3d45055e98d04430c61ea352393196ae02db7
knorth <kayci.north@gmail.com>
2022-08-20 13:03:11 -0600

    created schema

---
+++ models/mindful.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+//Create Schema
+const mindfulSchema = new mongoose.Schema({
+    date: {type: Number, required: true},
+    activityName: {type: String, required: true},
+    stateOfMindBeforeActivity: String,
+    stateOfMindAfterActivity: String,
+    waterConsumption: Number,
+    hoursSlept: Number,
+    postiveOccuranceOfTheDay: String,
+})
+
+const Mindful = mongoose.model('Mindful', mindfulSchema)
+
+module.exports.Mindful
