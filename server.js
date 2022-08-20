89e276afc119ec956fb92d07e4c6af8d3b54f5f1
knorth <kayci.north@gmail.com>
2022-08-20 13:16:13 -0600

    setup middleware

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ mongoose.connection.once('open', () => {
 });
 
 //middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
