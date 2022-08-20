27b97d06063a5e931fdca8636fe7cc5ef6eac3ac
knorth <kayci.north@gmail.com>
2022-08-20 13:14:19 -0600

    setup mongoose connection

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express')
 const app = express()
 const Mindful = require('./models/mindful.js')
 const methodOverride = require('method-override')
+
+//setup Mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/basiccrud');
+mongoose.connection.once('open', () => {
+	console.log('connected to mongo');
+});
+
+//middleware
