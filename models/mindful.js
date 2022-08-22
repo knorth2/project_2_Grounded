3a245734efbe9bf9834fb6e447a259f505022a84
knorth <kayci.north@gmail.com>
2022-08-22 14:43:53 -0600

    updated some variable names

--- models/mindful.js
+++ models/mindful.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 //Create Schema
 const mindfulSchema = new mongoose.Schema({
     date: {type: String, required: true},
-    name: {type: String, trim: true, required: true},
-    beforeActivity: {type: String, trim: true},
-    afterActivity: {type: String, trim: true},
+    beforeMovement: String,
     water: Number,
     sleep: Number,
-    postiveEvent: {type: String, trim: true},
+    movement: String,
+    afterMovement: String,
+    postiveEvent: String,
 })
 
 const Mindful = mongoose.model('Mindful', mindfulSchema)
