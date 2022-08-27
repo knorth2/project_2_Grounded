8cb2cbaeec1c72ff9f45ba1485945c5789965b9a
knorth <kayci.north@gmail.com>
2022-08-27 09:15:53 -0600

    grid css

--- server.js
+++ server.js
@@ -3,11 +3,36 @@ const app = express()
 const Mindful = require('./models/mindful.js')
 const methodOverride = require('method-override')
 
+// const session = require('express-session')
+
 
 // set up access to .env file
 require('dotenv').config()
 
 const PORT = process.env.PORT
+
+// //set up session with secret
+// const SESSION_SECRET = process.env.SESSION_SECRET
+// console.log('Here is the session secret')
+// console.log(SESSION_SECRET)
+
+// // use secret
+// app.use(session({
+// 	secret: SESSION_SECRET,
+// 	resave: false, 
+// 	saveUninitialized: false 
+// }))
+
+// // custom middleware to make currentUser available as
+// // a local variable on all ROUTES
+// app.use((req, res, next) => {
+// 	res.locals.currentUser = req.session.currentUser
+
+// 	// if (req.session.currentUser) {
+// 	//   res.locals.authenticated = true
+//   //}
+// 	next()
+// })
 
 //import mongoose
 const mongoose = require('mongoose')
@@ -26,6 +51,9 @@ app.use(methodOverride('_method'))
 
 const mindfulController = require('./controllers/mindfulController.js')
 app.use('/mindful', mindfulController)
+
+// const userController = require('./controllers/userController.js')
+// app.use('/users', userController)
 
 //default route
 app.get('/', (req, res)=>{
